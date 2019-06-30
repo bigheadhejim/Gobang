d680abb79a1686c56389d024bb3d39817ec43c15
bigheadhejim <bigheadhejim@qq.com>
2019-06-30 23:24:22 +0800

    code

--- src/Gobang.java
+++ src/Gobang.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created in 2019-06-05
  */
@@ -10,8 +12,8 @@ public class Gobang {
 	private static int BRD_WID = 15;
 	private static int WIN_CON = 5;
 	private char[][] nBoard;
-	private char playerClr = '●';
-	private char aiClr = '○';
+	private static char playerClr = '●';
+	private static char aiClr = '○';
 	
 	/**
 	 * Description:
@@ -45,7 +47,7 @@ public class Gobang {
 		//遍历棋盘。
 		for (int i = 0; i<BRD_WID; ++i) {
 			for (int j=0; j<BRD_WID; ++j) {
-				System.out.print(this.nBoard[i][j]);
+				System.out.print(this.nBoard[i][j] + " ");
 			}
 			System.out.print('\n');
 		}
@@ -62,57 +64,57 @@ public class Gobang {
 		//用于连续计算某个方向有多少个棋子。
 		int contiuneNum = 0;
 		//判断3点方向和9点方向
-		for(int i=0; (posX+i>0 && posX+i<BRD_WID); --i)
+		for(int i=-1; (posX+i>0 && posX+i<BRD_WID); --i)
 		{
 			if (nBoard[posX][posY]!=nBoard[posX+i][posY])
 				break;
 			else
 				++contiuneNum;
 		}
-		for(int i=0; (posX+i>0 && posX+i<BRD_WID); ++i)
+		for(int i=1; (posX+i>0 && posX+i<BRD_WID); ++i)
 		{
 			if (nBoard[posX][posY]!=nBoard[posX+i][posY])
 				break;
 			else
 				++contiuneNum;
 		}
-		if (contiuneNum >=5)
+		if (contiuneNum >=WIN_CON)
 			judgement = 1;
 				
 		//判断0点方向和6点方向
-		for(int j=0; (posX+j>0 && posX+j<BRD_WID); --j)
+		for(int j=-1; (posY+j>0 && posY+j<BRD_WID); --j)
 		{
 			if (nBoard[posX][posY]!=nBoard[posX][posY+j])
 				break;
 			else
 				++contiuneNum;
 		}
-		for(int j=0; (posX+j>0 && posX+j<BRD_WID); ++j)
+		for(int j=1; (posX+j>0 && posX+j<BRD_WID); ++j)
 		{
 			if (nBoard[posX][posY]!=nBoard[posX][posY+j])
 				break;
 			else
 				++contiuneNum;
 		}
-		if (contiuneNum >=5)
+		if (contiuneNum >=WIN_CON)
 			judgement = 1;
 		
 		//判断左上和右下45度角
-		for(int i=0, j=0; (posX+i>0 && posX+i<BRD_WID)&&(posY+j>0 && posY+j<BRD_WID); --i,++j)
+		for(int i=-1, j=-1; (posX+i>0 && posX+i<BRD_WID)&&(posY+j>0 && posY+j<BRD_WID); --i,--j)
 		{
 			if (nBoard[posX][posY]!=nBoard[posX+i][posY+j])
 				break;
 			else
 				++contiuneNum;
 		}
-		for(int i=0, j=0; (posX+i>0 && posX+i<BRD_WID)&&(posY+j>0 && posY+j<BRD_WID); ++i,--j)
+		for(int i=1, j=1; (posX+i>0 && posX+i<BRD_WID)&&(posY+j>0 && posY+j<BRD_WID); ++i,++j)
 		{
 			if (nBoard[posX][posY]!=nBoard[posX+i][posY+j])
 				break;
 			else
 				++contiuneNum;
 		}
-		if (contiuneNum >=5)
+		if (contiuneNum >=WIN_CON)
 			judgement = 1;
 		
 		//判断左下和右上45度角
@@ -130,7 +132,7 @@ public class Gobang {
 			else
 				++contiuneNum;
 		}
-		if (contiuneNum >=5)
+		if (contiuneNum >=WIN_CON)
 			judgement = 1;
 		
 		return judgement;
@@ -144,10 +146,10 @@ public class Gobang {
 		// TODO 自动生成的方法存根
 		Gobang game = new Gobang();
 		for (;;) {
-			if (0 != game.playerTurn()) {
+			if (0 != game.playerTurn(game)) {
 				break;
 			}
-			if (0 != game.aiTurn()) {
+			if (0 != game.aiTurn(game)) {
 				break;
 			}
 		}
@@ -157,7 +159,25 @@ public class Gobang {
 	 * AI正在下棋
 	 * @return 0表示游戏继续，其他表示游戏已定胜负。
 	 */
-	private int aiTurn() {
+	private int playerTurn(Gobang gobang) {
+		System.out.println("Player turn!");
+		int x = 0;
+		int y = 0;
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		System.out.println("locate x");
+		x = scan.nextInt();
+		System.out.println("locate y");
+		y = scan.nextInt();
+		
+		gobang.nBoard[x][y] = playerClr;
+		gobang.printBoard();
+		
+		if (0<judge(x, y)) {
+			System.out.println("Player win");
+			return 1;
+		}
+			
 		return 0;
 		// TODO 自动生成的方法存根
 		
@@ -165,10 +185,30 @@ public class Gobang {
 
 	/**
 	 * 玩家正在下棋
+	 * @param game 
 	 * @return 0表示游戏继续，其他表示游戏已定胜负。
 	 */
-	private int playerTurn() {
+	private int aiTurn(Gobang gobang) {
+		System.out.println("AI turn!");
+		int x = 0;
+		int y = 0;
+		//Scanner scan = new Scanner(System.in);
+		//System.out.println("locate x");
+		//x = scan.nextInt();
+		//System.out.println("locate y");
+		//y = scan.nextInt();
 		
+		x = (int) (Math.random()*BRD_WID);
+		y = (int) (Math.random()*BRD_WID);
+		
+		gobang.nBoard[x][y] = aiClr;
+		gobang.printBoard();
+		
+		if (0>judge(x, y)) {
+			System.out.println("AI win");
+			return -1;
+		}
+			
 		return 0;
 		// TODO 自动生成的方法存根
 		
